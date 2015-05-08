82f34828635622a759d75217263985f94ac88dda
A.P. Rajshekhar <randalap@redhat.com>
2015-05-08 13:49:27 +0530

    crane

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
-     )
+import crane
+
+
+_abs_dir = os.path.dirname(os.path.abspath(__file__))
+_req_path = os.path.join(_abs_dir, 'requirements.txt')
+requirements = open(_req_path).read()
+_test_req_path = os.path.join(_abs_dir, 'test-requirements.txt')
+test_requirements = open(_test_req_path).read()
+
+
+setup(
+    name='crane',
+    version=crane.version,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    url='http://www.pulpproject.org',
+    license='GPLv2+',
+    author='Pulp Team',
+    author_email='pulp-list@redhat.com',
+    description='docker-registry-like API with redirection, as a wsgi app',
+    install_requires=requirements,
+    tests_require=test_requirements,
+    test_suite='unittest2.collector',
+    package_data={
+        'crane': ['data/*.conf', 'templates/*.html', 'static/css/*', 'static/js/*',
+                  'static/fonts/*', 'static/img/*']
+    },
+)
